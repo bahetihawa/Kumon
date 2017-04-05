b17b758c46d6d6eed35c956d5539c30211615f10
bahetihawa <you@example.com>
2017-04-05 18:04:07 +0530

    footer text addedd

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,2 +1,10 @@
-
+<div class="navbar-inverse" style="
+     position: absolute;
+    bottom: 0;
+    width: 100%;">
+    <div class="container-fluid">
+        <i>Design & Maintained By : <a href="http://itcombine.com">ITCombine</a></i>
+        <i class="pull-right">For Queries Call Us @  <a>+91- 9818442666</a></i>
+    </div>
+</div>
 <script src="/js/script.js"></script>
