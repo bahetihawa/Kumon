7bcb34977b86bb9683764c9a0e25434d50778440
bahetihawa <you@example.com>
2017-06-02 17:32:45 +0530

    changes

---
+++ resources/views/include/searchLeft.blade.php
@@ -0,0 +1,8 @@
+<form class='row'>
+	<div class="col-md-8">
+		<input class='form-control input-sm col-md-5' name="leftSearch"/>
+	</div>
+	<div class="col-md-4">
+		<input type="submit" class='input-sm' value="Search" id= />
+	</div>	
+</forrm>
